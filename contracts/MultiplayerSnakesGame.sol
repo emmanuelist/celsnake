70b6c0931ef8301d0f63aebeb13c1cce51c85e70
emmanuelist <emmanuel.paul75@yahoo.com>
2026-04-02 17:43:38 +0100

    Add function to withdraw accumulated house fees for owner

--- contracts/MultiplayerSnakesGame.sol
+++ contracts/MultiplayerSnakesGame.sol
@@ -518,3 +518,11 @@ contract MultiplayerSnakesGame {
     function getContractBalance() external view returns (uint256) {
         return address(this).balance;
     }
+
+    /**
+     * @dev Withdraw accumulated house fees (owner only)
+     */
+    function withdraw(uint256 amount) external onlyOwner {
+        require(amount > 0 && amount <= address(this).balance, "Invalid amount");
+        payable(owner).transfer(amount);
+    }
