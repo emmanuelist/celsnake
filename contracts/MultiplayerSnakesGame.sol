25142136c5f6ea05f8330cce59d725b9598e1517
emmanuelist <emmanuel.paul75@yahoo.com>
2026-04-02 16:54:56 +0100

    Implement initial logic for _distributeProportional function to handle prize distribution

--- contracts/MultiplayerSnakesGame.sol
+++ contracts/MultiplayerSnakesGame.sol
@@ -336,3 +336,5 @@ contract MultiplayerSnakesGame {
         private 
         view 
         returns (address[] memory winners, uint256[] memory prizes)
+    {
+        Room storage room = rooms[roomId];
